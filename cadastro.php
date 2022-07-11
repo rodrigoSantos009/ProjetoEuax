0b211bffa25fdf217a7e1227601671a5bfae050a
Rodrigo <009rodrigo009@gmail.com>
2022-07-10 21:59:07 -0300

    Adicionado tela cadastro

---
+++ cadastro.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <?php
+
+    include_once 'menu_connected.php';
+
+    ?>
+
+    <main class="container">
+        <div class="card-login">
+
+            <form method="POST" action="valida_cadastro.php" class="form-cad">
+                <h2>Efetuar cadastro</h2>
+
+                <div class="input">
+                    <input type="email" name="email" required>
+                    <label>Email</label>
+                </div>
+
+                <div class="input">
+                    <input type="password" name="password" required>
+                    <label>Senha</label>
+                </div>
+
+                <div class="input">
+                    <input type="password" name="repeatpassword" required>
+                    <label>Confirmar senha</label>
+                </div>
+
+
+                <?php if (isset($_GET['login']) && $_GET['login'] == 'error3') {
+
+                ?>
+
+                    <div class="error">
+                        Senhas diferentes
+                    </div>
+
+                <?php } ?>
+
+                <?php if (isset($_GET['login']) && $_GET['login'] == 'error2') {
+
+                ?>
+
+                    <div class="error">
+                        Login necessário para acessar está página
+                    </div>
+
+                <?php } ?>
+
+                <div class="button">
+                    <button type="submit">Send</button>
+                </div>
+
+            </form>
+        </div>
+
+    </main>
+
+</body>
+
+</html>
