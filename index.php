a95d24c96b89469ae04634be43aef01751555a97
Rodrigo <009rodrigo009@gmail.com>
2022-07-10 21:54:00 -0300

    Adicionado tela inicial e estilos

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Início</title>
+
+</head>
+
+<body>
+
+    <header>
+        <img width="150px" src="img/euax-anywhere-logo.png" alt="">
+        <nav id="nav-links">
+            <ul id="menu">
+                <li><a href="login.php">Login</a></li>
+                <li><a href="cadastro.php">Cadastre-se</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="container">
+
+        <h1>Help Euax</h1>
+
+        <div class="card">
+            <div class="text">
+                <h2>A ferramenta <br>Perfeita para sua empresa</h2>
+                <p>Tenha uma melhor organização em sua rotina de trabalho com nossa ferramenta de chamados</p>
+            </div>
+        </div>
+    </main>
+
+
+</body>
+
+</html>
